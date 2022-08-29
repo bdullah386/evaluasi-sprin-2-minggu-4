102735c9832bc26cce28f1cdccef969e490cf131
bdullah386 <bdullahbdullah386@gmail.com>
2022-08-29 10:46:56 +0700

    evaluasi

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// function formatphone(...params){
+//     // console.log(params);
+//     paramsOne = [0,8,5,4,5]
+//     paramsTwo = [6,7,8,9,0]
+//     paramsThre= [...paramsOne, ...paramsTwo]
+//     console.log(...paramsThre);
+// }
+
+// function formatphone(...number){
+//     console.log(number); 
+// }
+
+// function hitunggaji(){[
+//     'nama','jabatan','gaji perhari'
+// ]}
+
+
+
+// let ketua = [{nama:'huda',jabatan: 'kepala chef',gajiperhari:100000,absen:'2 hari',gaji:2800000}]
+// let asisten = [{nama:'abdullah',jabatan: 'asisten chef',gajiperhari:90000,absen:'3 hari',gaji:2430000}]
+// let kasir = [{nama:'imran',jabatan: 'kasir',gajiperhari:50000,absen:'1 hari',gaji:1450000}]
+// let pelayan = [{nama:'fajar',jabatan: 'pelayan',gajiperhari:50000,absen:'1 hari',gaji:1450000}]
+// let hasil = [...ketua, ...asisten, ...kasir, ...pelayan]
+// console.table(hasil);
+
+// let gajiHuda = 100000 * 28
+// console.log(gajiHuda);
+// let gajiAbdullah = 90000 * 27
+// console.log(gajiAbdullah);
+// let gaji = 50000 * 29
+// console.log(gaji);
+
+
+let gaji = []
+function hitunggaji(namakaryawan, absen){
+    namakaryawan;
+    absen
+}
+
+function nama(namakaryawan){
+    100000 * 28
+}
